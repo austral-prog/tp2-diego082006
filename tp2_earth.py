7ad073fc2d5bbaf4af4aab44fb442b4973490f5d
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-08-22 11:51:03 -0300

    TP2 - Diego Delgado
    Ejercicio 1 - ADA
    Ejercicio 2 - EARTH
    Ejercicio 3 - CHANGE

---
+++ tp2_earth.py
@@ -0,0 +1,11 @@
+def earth():
+    x = "Bangladesh"
+    y = "Barbados"
+    
+    x_comes_first = x < y
+    y_comes_first = y < x
+    
+    print(f"The result of {x} comes first in the dictionary than {y} is {x_comes_first}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {y_comes_first}.")
+
+earth()
