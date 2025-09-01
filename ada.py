a2f969374c0e8e88a33e1a2bc69bc3d656f4e82d
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-09-01 12:43:20 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = f"{first_name} {last_name}"
+    print(full_name.lower())
+    print(full_name.title())
+    print(full_name.upper())
+    print("\t" + full_name.lower())
+    
+ada()
